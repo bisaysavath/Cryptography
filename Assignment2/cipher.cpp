215b08f8a88f581e348466931b51c688f2128f09
Duy Do <ddo@csu.fullerton.edu>
2016-04-17 21:07:03 -0700

    added code to remove padding when derypting

--- Assignment2/cipher.cpp
+++ Assignment2/cipher.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
 #include "CipherInterface.h"
 #include "DES.h"
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
 	
 	/* Check number of arguments */
-	if (argc != 6 )
-		{
-			cout << "Not enough arguments" <<endl;
-			cout << "Usage: " << argv[0] <<" DES <KEY> <ENC/DEC> <INPUTFILE> <OUTPUT FILE>" <<endl;
-			exit (-1);
-		}
+	if (argc != 6 ){
+		cout << "Not enough arguments" <<endl;
+		cout << "Usage: " << argv[0] <<" DES <KEY> <ENC/DEC> <INPUTFILE> <OUTPUT FILE>" <<endl;
+		exit (-1);
+	}
     
 	string cipherName = argv[1];
 	char* key = argv[2];
@@ -27,75 +26,110 @@ int main(int argc, char** argv)
 	CipherInterface* cipher = new DES();
 	
 	/* Error checks */
-	if(!cipher)
-		{
-			fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",	
-				__FILE__, __FUNCTION__, __LINE__);
-			exit(-2);
-		}
+	if(!cipher){
+		fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",	
+			__FILE__, __FUNCTION__, __LINE__);
+		exit(-2);
+	}
 	
 	/* Set the encryption key
 	 * A valid key comprises 16 hexidecimal
 	 */
-	if(!cipher->setKey((unsigned char*)key))
-		{
-			cout << "ERROR: Key should have 16 hexadecimal digits." << endl;
-			exit(-3);
-		}
+	if(!cipher->setKey((unsigned char*)key)){
+		cout << "ERROR: Key should have 16 hexadecimal digits." << endl;
+		exit(-3);
+	}
     
 	/* Open input file */
    
 	FILE* inFile;
 	FILE* outFile;
 	inFile = fopen(inputFile, "r");
-	outFile = fopen(outputFile, "w");
-	if ( inFile == NULL )
-		{
-			cout << "ERROR: Could not open input file" << endl;
-			exit(-3);
-		}
+	outFile = fopen(outputFile, "w+");
+	if ( inFile == NULL ){
+		cout << "ERROR: Could not open input file" << endl;
+		exit(-4);
+	}
 
-	if ( outFile == NULL )
-		{
-			cout << "ERROR: Could not open output file" << endl;
-		}
+	if ( outFile == NULL ){
+		cout << "ERROR: Could not open output file" << endl;
+		exit(-5);
+	}
 	
 	int size;	
 	unsigned char* cipherText;
 	unsigned char text[8];
 	unsigned char* decryptedText;
-	while ( (size = fread(text, 1, 8, inFile)) != 0 )
-		{
+	int padding = 0;
+	while ( (size = fread(text, sizeof(char), 8, inFile)) != 0 ){
 
-		        while ( size != 8 ){
+		while ( size != 8 ){
+			/* Padding */
+			padding++;
 
-				/* Padding */
-				text[size] = 'x'; 
-				size++;
+			if ( size == 7 ){
+				text[size] = padding + '0';
 			}
-			if ( mode == "ENC" ){
-				
-				/* Perform encryption */
-				cipherText = cipher->encrypt((unsigned char*)text);
-				printf("%s\n", cipherText);
-				fwrite(cipherText, 1, 8, outFile);
-				free(cipherText);
+			else {
+				text[size] = '0';
 			}
-
-			if ( mode == "DEC" ){
+			size++;
+		}
+			
+		if ( mode == "ENC" ){
 				
-				/* Perform decryption */		
-				decryptedText = cipher->decrypt((unsigned char*)text);	
-				printf("%s\n", decryptedText);
-				fwrite(decryptedText, 1, 8, outFile);
-				free(decryptedText);
-				
-			}
+			/* Perform encryption */
+			cipherText = cipher->encrypt((unsigned char*)text);
+			printf("%s", cipherText);
+			fwrite(cipherText, sizeof(char), 8, outFile);
+			free(cipherText);
 		}
 
+		if ( mode == "DEC" ){
+				
+			/* Perform decryption */		
+			decryptedText = cipher->decrypt((unsigned char*)text);	
+			printf("%s", decryptedText);
+			fwrite(decryptedText, sizeof(char), 8, outFile);
+			free(decryptedText);
+				
+		}
+	}
+
+	/* Remove padding for DES decryption */
+	if ( mode == "DEC" ){
+
+		long int outFileSize = ftell(outFile);
+		unsigned char lastBlock[8];
+
+		/* Go to the last block of the output file */
+		fseek(outFile, -8, SEEK_END);
+		
+		/* Read in the content of the last block */
+		fread(lastBlock, sizeof(char), 8, outFile);
+
+		/* Examine the last character */
+		padding = (int) (lastBlock[7] - '0');
+		bool isPadding = true;
+		if ( padding > 0 && padding < 8 ){
+			
+			/* Verify the padding */
+			while ( padding > 1 ){
+				if ( lastBlock[8-padding] != '0' ) isPadding = false;
+				padding--;
+			}
+
+			if ( isPadding ){
+				
+				/* Truncate the output file */
+				ftruncate(fileno(outFile), outFileSize - (int)(lastBlock[7] - '0') );		        
+			}
+		}
+	}
+	
         fclose(inFile);
 	fclose(outFile);
-	cout << "Success!!" <<endl;
+	cout << "\nSuccess!!" <<endl;
 	
 	return 0;
 }
